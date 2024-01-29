633905aed1d3b196e6382369a4db9e9c1d2d32a8
Akliluu <aklilunebi05@gmail.com>
2024-01-29 10:56:00 +0100

    1

---
+++ ModifieraIteration.php
@@ -0,0 +1,8 @@
+<html> 
+    <?php
+        $siffror = array("ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio");
+        for ($i = 1; $i <= 10; $i++) {
+            echo "Rad ".$siffror[$i-1] . "<br>";
+        }
+    ?>
+</html>
