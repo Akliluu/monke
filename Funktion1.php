8d470864fe48ce256341d8ab80854eb1f0bd0d93
Akliluu <aklilunebi05@gmail.com>
2024-05-27 10:44:41 +0200

    yes

---
+++ Funktion1.php
@@ -0,0 +1,12 @@
+<?php
+// Definiera funktionen
+function summa($tal1, $tal2) {
+    return $tal1 + $tal2;
+}
+
+// Anropa funktionen med de värden du vill addera
+$resultat = summa(6, 9);
+
+// resultatet
+echo "Summan av 6 och 9 är: " . $resultat;
+?>
